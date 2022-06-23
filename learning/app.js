b80ebf9efa3c8758c64c54ea261db3b75c8942d0
nikhilkumarmca <nikhilkumarmca1821@gmail.com>
2022-06-23 17:04:25 +0530

    express

--- learning/app.js
+++ learning/app.js
@@ -1,2 +1,30 @@
-const os = require('os')
-console.log(os.freemem());
+// const os = require('os')
+// console.log(os.freemem());
+
+const express = require('express');
+
+const app = express();
+app.get('/',function(req,res){
+    res.send('Hello world')
+})
+
+app.get('/student/:id',function(req,res){
+    const id = req.params.id
+    if (id==121)
+    {
+        res.send('welcome nikhil')
+    }
+    else if(id==122)
+    {
+        res.send('hey subham')
+    }
+    else
+    {
+        res.send('no data found')
+    }
+    
+})
+
+app.listen(9000, function(req,res){
+    console.log('Running....');
+});

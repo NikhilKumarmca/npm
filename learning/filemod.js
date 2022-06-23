d41105a95f3e239431bc6c9b2a43d29075a221a7
nikhilkumarmca <nikhilkumarmca1821@gmail.com>
2022-06-23 15:31:49 +0530

    work on module

--- learning/filemod.js
+++ learning/filemod.js
@@ -1,9 +1,23 @@
 var fs = require('fs')
 
-fs.readFile('calc.js','utf8',function(err, data){
-    console.log(data);
-})
 
-fs.writeFile('newcalc.js','console.log("done")',function(err){
-    console.log("file created");
+// ==========to read file===============
+// fs.readFile('calc.js','utf8',function(err, data){
+//     console.log(data);
+// })
+
+
+//===========to write file===============
+// fs.writeFile('newcalc.js','console.log("done")',function(err){
+//     console.log("file created");
+// })
+
+//===========to append file=============
+// fs.appendFile('newcalc.js','\nconsole.log("new msg is here")',function(err){
+//     console.log("added 1 line");
+// })
+
+//===========to delete file============
+fs.unlink('newcalc.js',function(err){
+    console.log("file deleted");
 })
